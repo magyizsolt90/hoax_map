1eaf4df082c9936c2894b29a16f9cac877f27c87
Zsolt Magyi <magyi.zsolt90@gmail.com>
2024-08-27 06:35:12 +0200

    app store + google play footer fix

--- src/components/Stores.tsx
+++ src/components/Stores.tsx
@@ -2,32 +2,40 @@ import React from "react";
 
 export default function Stores() {
   return (
-    <div className="flex flex-col items-center justify-center gap-2 absolute bg-black bottom-0 left-0 w-full h-[80px] ">
-      <span className="font-rubik font-semibold">többre is kíváncsi vagy?</span>
+    <div className="flex flex-col items-center justify-center gap-2 absolute bg-yellow bottom-5 left-0 w-full h-[80px]">
+      <span className="font-rubik font-semibold text-white">
+        többre is kíváncsi vagy?
+      </span>
       <div className="flex flex-row items-center justify-center gap-5">
-        <a
-          href="https://apps.apple.com/app/hoax-coffee/id6443516503"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            className="w-[100px] h-[30px]"
-            src="/icons/apple.svg"
-            alt="Download on the App Store"
-          />
-        </a>
+        
+        <div className="bg-white p-2 rounded-full flex items-center justify-center w-[140px] h-[50px]">
+          <a
+            href="https://apps.apple.com/app/hoax-coffee/id6443516503"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              className="w-[100px] h-auto"
+              src="/icons/btn_ios.svg"
+              alt="Download on the App Store"
+            />
+          </a>
+        </div>
 
-        <a
-          href=" https://play.google.com/store/apps/details?id=com.hoax.hoax"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            className="w-[100px] h-auto"
-            src="/icons/google.png"
-            alt="Get it on Google Play"
-          />
-        </a>
+        <div className="bg-white p-2 rounded-full flex items-center justify-center w-[140px] h-[50px]">
+          <a
+            href="https://play.google.com/store/apps/details?id=com.hoax.hoax"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              className="w-[100px] h-auto"
+              src="/icons/btn_android.svg"
+              alt="Get it on Google Play"
+            />
+          </a>
+        </div>
+
       </div>
     </div>
   );
